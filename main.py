9da4fc6f0842bc504d7aac203b1d6804dbb28aeb
Pattharamon D. <pattharamon.d@ku.th>
2024-07-15 00:23:51 +0700

    test

---
+++ main.py
@@ -0,0 +1,23 @@
+num = int(input())
+count = 0
+ace = False
+ten = False
+for i in range(num):
+    text = list(str(input()))
+    text.pop()
+    text = "".join(text)
+    if text.isnumeric():
+        if text == "10":
+            ten = True
+        count += int(text)
+    elif text in ["J", "Q", "K"]:
+        ten = True
+        count += 10
+    elif text == "A":
+        ace = True
+        if count+11 > 21:
+            count += 1
+        else: count += 11
+print("Blackjack") if ace is True and ten is True \
+    else print("Bust") if count > 21 \
+    else print(count, "points")
